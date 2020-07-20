28782820df12b7b18d2171dbb29e044896d7d9c9
blackbug-official <68537298+blackbug-official@users.noreply.github.com>
2020-07-20 14:55:16 +0530

    Added Fio project

---
+++ Fight It Out (Webinar)/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public ParticleSystem DeathFx;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(DeathFx, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
